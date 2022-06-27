e05ae3ee2a8368d5b4ddc3a79d43a88f55271c4f
Amel Jasari <108287926+amel987@users.noreply.github.com>
2022-06-27 19:24:19 +0200

    Add files via upload

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,36 @@
 const { defineConfig } = require("cypress");
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
+
 
 
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
 
-      defaultCommandTimeout: 25000
+      defaultCommandTimeout: 25000,
+      //require('cypress-mochawesome-reporter/plugin')(on);
 
 
-      // implement node event listeners here
+
+    on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+      });
+
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      }); 
     },
-    
+
     baseUrl: 'https://github.com/',
 
+    defaultCommandTimeout: 25000,
   },
 });
+    
+  
+
+
